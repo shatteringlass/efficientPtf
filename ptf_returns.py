fece7591fc58df9142803daea2709c1ffab4e2e3
shatteringlass <federico.pizzolo@gmail.com>
2017-10-07 14:28:00 +0200

    various fixes

--- ptf_returns.py
+++ ptf_returns.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
 
+epsilon = 1e-4
+
 try:
     dim = int(input('How many risky assets are present in the market?\n'))
 except ValueError:
@@ -14,7 +16,7 @@ else:
     raise SystemExit("No input file provided. Exiting...")
 
 cov_path = input('Provide the path to the covariance matrix (csv format only) for the risky assets: ')
-if mean_path != "":
+if cov_path != "":
     V = np.loadtxt(open(cov_path, 'rb'), delimiter=',')
     assert len(V[0]) == dim
 else:
@@ -24,19 +26,25 @@ try:
     rf_rate = float(input('State the riskfree rate on the market (in p.p.): '))
 except ValueError:
     raise SystemExit()
+
 try:
     req_vol = float(input('State the required max volatility for the portfolio to be built (in p.p.): '))
 except ValueError:
     raise SystemExit()
 
+ptf_weights = input('Provide the path to the portfolio weights (csv format only - leave blank for a uniformly distributed portfolio): ')
+if ptf_weights != "":
+    ptf = np.loadtxt(open(ptf_weights, 'rb'), delimiter=',')
+else:
+    ptf = np.ones((dim,1))/dim
+assert len(ptf) == dim and 1-sum(ptf)<epsilon
+
+print('\nAbout to compute returns for selected portfolio with weights %s\n' % ptf)
+
 excess_ret = mu - rf_rate
-sharpe_ptf = np.linalg.solve(V, excess_ret)
-sharpe_ptf = sharpe_ptf / sum(sharpe_ptf)
-nom = np.dot(sharpe_ptf, mu) - rf_rate
-denom = 100 * (math.sqrt(np.dot(sharpe_ptf, np.matmul(V, sharpe_ptf))))
-beta = nom / denom
+nom = np.dot(ptf, mu) - rf_rate
+denom = 100 * (math.sqrt(np.dot(ptf, np.matmul(V, ptf))))
+beta = (nom) / denom
 rate = rf_rate + beta * req_vol
 
-ptf_string = np.array_str(sharpe_ptf)
-
-print('\nThe Sharpe ptf has weights:\n\n\t%s\n\nand its rate of return amounts to:\n\n\t%f\n' % (ptf_string, rate))
+print('\nThe selected portfolio has rate of return:\n\n\t%f\n' % rate)
--- var.py
+++ var.py
@@ -1,6 +1,6 @@
-import math
 import numpy as np
-import os
+
+epsilon = 1e-4
 
 try:
     dim = int(input('How many risky assets are present in the market?\n'))
@@ -9,20 +9,20 @@ except ValueError:
 
 hist_ret = input('Provide the path to the historical returns (csv format only) for the risky assets: ')
 if hist_ret != "":
-    hmu = np.loadtxt(open(hist_ret, 'rb'), delimiter=',').reshape(-1,dim)
+    hmu = np.loadtxt(open(hist_ret, 'rb'), delimiter=',').reshape(-1, dim)
     print('\n %s \n' % hmu)
     assert len(hmu[0]) == dim
 else:
     raise SystemExit("No input file provided. Exiting...")
 
-ptf_weights = np.array(input('Provide the portfolio weighting (leave blank for a uniform portfolio):') or np.zeros(dim)+1/dim)
-assert len(ptf_weights) == dim and int(sum(ptf_weights)) == 1
+ptf_weights = np.array(eval(input('Provide the portfolio weighting (leave blank for a uniform portfolio):')) or np.zeros(dim) + 1 / dim)
+assert len(ptf_weights) == dim and 1-int(sum(ptf_weights)) <= epsilon
 
 print('\nSelected portfolio has weights %s\n' % np.array_str(ptf_weights))
 
 # Evaluate daily return for selected ptf
 
-daily_return = hmu.dot(ptf_weights.T).reshape(-1,1)*-1
+daily_return = hmu.dot(ptf_weights.T).reshape(-1, 1) * -1
 hist_days = len(daily_return)
 
 print('Daily losses are as follows:\n %s \n' % daily_return)
@@ -37,7 +37,7 @@ var = np.percentile(daily_return, q=conf, interpolation='higher')
 
 print('\nValue at Risk estimated is equal to: %s' % var)
 
-cvar = daily_return[daily_return >= var].sum()/(hist_days*(1-conf/100))
+cvar = daily_return[daily_return >= var].sum() / (hist_days * (1 - conf / 100))
 assert cvar >= var
 
 print('\nConditional Value at Risk (cVaR or expected shortfall) estimated is equal to: %s' % cvar)
